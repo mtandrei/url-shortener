a1bae220f5e4f6aa104c2973599de95fabf2e8d8
Mihai Andrei <mtandrei@gmail.com>
2016-02-18 00:01:51 -0500

    Added error handling

--- url/models.py
+++ url/models.py
@@ -3,7 +3,7 @@ from url import db
 class URLMap(db.Model):
     __tablename__ = 'urls'
     id = db.Column(db.Integer, primary_key=True)
-    longURL = db.Column(db.String(500), index=True, unique=True)
+    longURL = db.Column(db.String(500), index=True)
     shortURL = db.Column(db.String(20), index=True, unique=True)
 
     def __repr__(self):
--- url/views.py
+++ url/views.py
@@ -1,7 +1,8 @@
-from flask import render_template, flash, redirect, request
+from flask import render_template, flash, redirect, request, url_for
 from url import app, db
 from url.models import URLMap
 from urllib2 import urlopen
+import random 
 
 @app.route('/')
 @app.route('/index', methods=['POST', 'GET'])
@@ -11,30 +12,31 @@ def index():
     if request.method == 'POST':
         longURL = request.form['longURL']
         shortURL = request.form['shortURL']
+        # Try adding prefix
+        if not longURL.startswith('http://') and not longURL.startswith('https://'):
+            longURL = 'http://' + longURL
+
         ret = urlopen(longURL)
         if ret.code >= 400:
-            # try adding http
-            longURL = 'http://' + longURL
-            ret = urlopen(longURL)
-            if ret.code >= 400:
-                return render_template('index.html', error=error)# ERROR
+            return render_template('index.html', error=error)# ERROR
+
+        if shortURL == "":
+            shortURL = get_random_string()
 
         url = URLMap(longURL=longURL, shortURL=shortURL)
         db.session.add(url)
         db.session.commit()
-        flash("loL!!!!")
+        flash("Your shortened URL is " + url_for('red', url=shortURL, _external=True))
         return render_template('index.html', url=url)
 
 @app.route('/<url>', methods=['GET'])
 def red(url):
     if request.method == 'GET':
         url_map = URLMap.query.filter_by(shortURL=url).first()
-        youareel = url_map.longURL
-        #return youareel
-        return redirect(youareel)
+        if url_map is None:
+            return render_template('error.html')
+        return redirect(url_map.longURL)
 
 
-def url_to_json(url):
-    return {'id': url.id,
-            'longURL': url.longURL,
-            'shortURL': url.shortURL }
+def get_random_string(length=7,allowed_chars='abcdefghigklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'):
+    return ''.join(random.choice(allowed_chars) for i in range(length))
